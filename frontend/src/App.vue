44fe767dc372688a85a68abad65314aac6103c9f
florimondmanca <florimond.manca@gmail.com>
2019-06-11 23:25:32 +0200

    Add API call to shorten url

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,26 @@
   <div id="app">
     <h1>URL Shortener</h1>
     <h3>Simplify your links</h3>
-    <form>
-      <input type="text" name="url" placeholder="Enter original URL here">
+    <form @submit="onSubmit">
+      <input type="text" name="url" placeholder="Enter original URL here" required>
       <button type="submit">Shorten URL</button>
     </form>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      fetch("http://localhost:8000/urls", { method: "POST" })
+        .then(res => res.json())
+        .then(console.log);
+    }
+  }
+};
+</script>
+
 
 <style>
 #app {
