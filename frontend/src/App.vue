fa0037256b5268fe8e23a80bae77e7b98c5e772e
florimondmanca <florimond.manca@gmail.com>
2019-06-12 22:10:35 +0200

    Update styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -22,12 +22,9 @@ export default {
 };
 </script>
 
-
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
