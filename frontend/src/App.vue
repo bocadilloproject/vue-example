a3c09dc6026d9ce5f79557a0ed2715194b4b1b33
florimondmanca <florimond.manca@gmail.com>
2019-06-12 23:48:35 +0200

    Bind url to input element

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
     <h1>URL Shortener</h1>
     <h3>Simplify your links</h3>
     <form @submit="onSubmit">
-      <input type="text" name="url" placeholder="Enter original URL here" required>
+      <input v-model="url" type="text" name="url" placeholder="Enter original URL here" required>
       <button type="submit">Shorten URL</button>
     </form>
   </div>
@@ -11,10 +11,16 @@
 
 <script>
 export default {
+  data() {
+    return { url: "" };
+  },
   methods: {
     onSubmit(e) {
       e.preventDefault();
-      fetch("http://localhost:8000/urls", { method: "POST" })
+      fetch("http://localhost:8000/urls", {
+        body: { url: this.url },
+        method: "POST"
+      })
         .then(res => res.json())
         .then(console.log);
     }
