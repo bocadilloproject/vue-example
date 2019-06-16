8f89030de3e84cb93148b1ee6ca5068f44bd2f18
florimondmanca <florimond.manca@gmail.com>
2019-06-16 13:14:38 +0200

    Setup vue-router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,8 @@
 <template>
   <div id="app">
-    <h1>URL Shortener</h1>
-    <h3>Simplify your links</h3>
-    <form @submit="onSubmit">
-      <input v-model="url" type="text" name="url" placeholder="Enter original URL here" required>
-      <button type="submit">Shorten URL</button>
-    </form>
-    <div v-if="shortUrl">
-      <p>Shortened URL:</p>
-      <p>
-        <code>{{ shortUrl }}</code>
-      </p>
-    </div>
+    <router-view></router-view>
   </div>
 </template>
-
-<script>
-import { postJSON, makeFullUrl } from "./utils";
-export default {
-  data() {
-    return { url: "", shortUrl: null };
-  },
-  methods: {
-    onSubmit(e) {
-      e.preventDefault();
-      postJSON("/urls", { url: this.url }).then(
-        ({ hash }) => (this.shortUrl = makeFullUrl(`/urls/${hash}`))
-      );
-    }
-  }
-};
-</script>
 
 <style>
 #app {
