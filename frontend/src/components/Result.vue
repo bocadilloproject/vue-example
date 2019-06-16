e17b0bfce52debad1cf190efe44abd69f8a86e0f
florimondmanca <florimond.manca@gmail.com>
2019-06-16 15:14:23 +0200

    Add copy to clipboard

--- frontend/src/components/Result.vue
+++ frontend/src/components/Result.vue
@@ -3,10 +3,15 @@
     <h1 class="title">Shortened URL</h1>
     <div class="field is-grouped">
       <div class="control is-expanded">
-        <input type="text" class="input" :value="url" readonly>
+        <input type="url" class="input" :value="url" id="result" readonly>
       </div>
       <div class="control">
-        <button type="button" class="button is-info">Copy</button>
+        <button
+          type="button"
+          class="button is-info"
+          data-clipboard-target="#result"
+          @click="copy"
+        >Copy</button>
       </div>
     </div>
     <section class="hero">
@@ -18,8 +23,19 @@
 </template>
 
 <script>
+import Clipboard from "clipboard/dist/clipboard.min.js";
+
 export default {
   props: ["hash"],
+  data() {
+    return { clipboard: null };
+  },
+  mounted() {
+    this.clipboard = new Clipboard(".button");
+  },
+  destroyed() {
+    this.clipboard && this.clipboard.destroy();
+  },
   computed: {
     path() {
       return `/${this.hash}`;
