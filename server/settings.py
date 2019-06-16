2ccd9edb0935eaa97c5f164f41bb133c82e3b7ba
florimondmanca <florimond.manca@gmail.com>
2019-06-16 11:45:57 +0200

    setup dev CORS

--- server/settings.py
+++ server/settings.py
@@ -1,3 +1,9 @@
 from starlette.config import Config
 
 config = Config(".env")
+
+CORS = {
+    "allow_origins": ["http://localhost:8080"],
+    "allow_methods": ["*"],
+    "allow_headers": ["*"],
+}
